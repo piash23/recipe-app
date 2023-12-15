3bc33e502253f1772eeccda1d2a39cb77877c8d1
Md. Shihab Uddin <128211124+piash23@users.noreply.github.com>
2023-12-15 14:16:01 +0000

    add modyfying custom users model

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,6 +4,7 @@ Django admin customizations
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from core import models
+from django.utils.translation import gettext as _  # future proofing
 
 
 # @admin.register(models.User)
@@ -13,6 +14,21 @@ class UserAdmin(BaseUserAdmin):
     """
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password')
+        }),
+        (_('Personal Info'), {
+            'fields': ('name',)
+        }),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser')
+        }),
+        (_('Important dates'), {
+            'fields': ('last_login',)
+        })
+    )
+    readonly_fields = ('last_login',)
 
 
 admin.site.register(models.User, UserAdmin)
