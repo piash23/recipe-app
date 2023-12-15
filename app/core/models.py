96dd5521aca4aa70ec25767275cb7cf104d74a31
Md. Shihab Uddin <128211124+piash23@users.noreply.github.com>
2023-12-15 09:21:56 +0000

    added custom user model with tests

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,59 @@
-from django.db import models # noqa
+"""
+Database models
+"""
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """
+    Custom user manager
+    """
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create and save a new user
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+        # self.normalize_email is a helper function that comes with the
+        # BaseUserManager that will normalize the second half of the email
+        # address to the lower case.
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        # self.model is a helper function that comes with the BaseUserManager
+        # that will create a new user model.
+        user.set_password(password)
+        user.save(using=self._db)
+        # self._db is a helper function that comes with the BaseUserManager
+        # that will allow you to support multiple databases.
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Create and save a new superuser
+        """
+        user = self.create_user(email, password)
+        # self.create_user is a function that we created above.
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        # self._db is a helper function that comes with the BaseUserManager
+        # that will allow you to support multiple databases.
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    User in the system
+    """
+
+    objects = UserManager()
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)  # required for custom user
+    is_staff = models.BooleanField(default=False)  # required for custom user
+    # is_staff is a field that determines whether the user is a staff user.
+
+    USERNAME_FIELD = 'email'  # required for custom user model
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -21,3 +21,32 @@ class ModelTests(TestCase):
         # It's the recommended way of retrieving the user model
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        sample_email = [
+            ('test1@EXAMPLE.com', 'test1@example.com'),
+            ('Test2@Example.com', 'Test2@example.com'),
+            ('TEST3@EXAMPLE.COM', 'TEST3@example.com'),
+            ('test4@example.COM', 'test4@example.com'),
+        ]
+        
+        for email, expected in sample_email:
+            user = get_user_model().objects.create_user(email, 'test123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raise_error(self):
+        """
+        Test that creating a user without an email raises a ValueError
+        """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+            )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
